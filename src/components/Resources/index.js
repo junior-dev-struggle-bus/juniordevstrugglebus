d7d9fdd94ba64dd223047f0c3f32b92721fe6c5b
Nate Spilman <Nate.Spilman@gmail.com>
2019-04-08 13:28:30 -0700

    testing Netlify custom variables"

--- src/components/Resources/index.js
+++ src/components/Resources/index.js
@@ -35,6 +35,12 @@ class Resources extends Component{
         const {categories} = this.state
         return(
             <div className = "container">
+             <p>NODE_ENV value is "{process.env.NODE_ENV}"</p>
+        <p>CUSTOM_ENV_VAR value is "{process.env.CUSTOM_ENV_VAR}"</p>
+        <p>REACT_APP_CUSTOM_ENV_VAR value is "{process.env.REACT_APP_CUSTOM_ENV_VAR}"</p>
+        <p>TOML_ENV_VAR value is "{process.env.TOML_ENV_VAR}"</p>
+        <p>REACT_APP_TOML_ENV_VAR value is "{process.env.REACT_APP_TOML_ENV_VAR}"</p>
+ 
             {categories.map(category =>{ 
                 const resources = this.state.resources.filter(resource => 
                     resource['language/topic'] == category)
