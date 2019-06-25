725eb586d5973c5f4c882de2377105715b809ffa
Brett Hurst <bretthurst@outlook.com>
2019-06-25 12:38:46 -0700

    Removed unused react router Switch import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "bootstrap";
 import "./App.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
