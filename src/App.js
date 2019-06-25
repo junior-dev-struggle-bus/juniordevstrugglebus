6230ede4cfb8b950a0f90208adc3053afb8f7731
Brett Hurst <bretthurst@outlook.com>
2019-06-25 12:00:13 -0700

    Removed logo.svg import form App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
-import 'bootstrap';
-import logo from './logo.svg';
-import './App.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import "bootstrap";
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
-import './css/socicon.css';
-import './css/iconsmind.css';
-import './css/interface-icons.css';
-import './css/lightbox.min.css';
-import './css/theme.css';
-import './css/custom.css';
+import "./css/socicon.css";
+import "./css/iconsmind.css";
+import "./css/interface-icons.css";
+import "./css/lightbox.min.css";
+import "./css/theme.css";
+import "./css/custom.css";
 
-import Home from './components/Home/';
-import Header from './components/Header/Header';
-import Resources from './components/Resources/';
-import Events from './components/Events/';
-import Organization from './components/Organization/';
+import Home from "./components/Home/";
+import Header from "./components/Header/Header";
+import Resources from "./components/Resources/";
+import Events from "./components/Events/";
+import Organization from "./components/Organization/";
 
 class App extends Component {
   render() {
