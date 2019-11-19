3d892e6726164c02674a319cb05ef5216e2759c6
Xoadra <xijp77@gmail.com>
2019-11-18 23:58:20 -0800

    Form page for resource submission added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import "bootstrap";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap";
+
+import Home from "./components/Home";
+import Header from "./components/Header/Header";
+import Resources from "./components/Resources";
+import Events from "./components/Events";
+import Organization from "./components/Organization";
+import NotFound from "./components/NotFound";
 
 import "./css/socicon.css";
 import "./css/iconsmind.css";
@@ -10,34 +17,31 @@ import "./css/lightbox.min.css";
 import "./css/theme.css";
 import "./css/custom.css";
 
-import Home from "./components/Home/";
-import Header from "./components/Header/Header";
-import Resources from "./components/Resources/";
-import Events from "./components/Events/";
-import Organization from "./components/Organization/";
-import NotFound from "./components/NotFound";
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Router>
-          <Header link={Link} />
-          <Switch>
-            <Route path="/" exact component={Home} />
-            <Route path="/resources" exact component={Resources} />
-            <Route path="/events" exact component={Events} />
-            <Route path="/organization" exact component={Organization} />
-            <Route path='/slack' component={() => { 
-              window.location.href = 'https://join.slack.com/t/jdsb/shared_invite/enQtNzA0NTY3OTE2ODg3LTE5ZTE5ODI5YmE5YTUzN2UyOWUxZmM1ZDZlNDliZTgxYTg0ODRlMmM3OThkY2JlZDRlNjIzYmJiMjNjNDBjOWQ'; 
-            return null;
-}}/>
-            <Route component={NotFound} />
-          </Switch>
-        </Router>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <Router>
+                    <Header link={Link} />
+                    <Switch>
+                        <Route path="/" exact component={Home} />
+                        <Route path="/resources" component={Resources} />} />
+                        <Route path="/events" exact component={Events} />
+                        <Route path="/organization" exact component={Organization} />
+                        <Route path="/slack" render={() => {
+                            window.location.href = "https://join.slack.com/t/jdsb/shared_invite/enQtNzA0NTY3OTE2ODg3LTE5ZTE5ODI5YmE5YTUzN2UyOWUxZmM1ZDZlNDliZTgxYTg0ODRlMmM3OThkY2JlZDRlNjIzYmJiMjNjNDBjOWQ"; 
+                            return null;
+                        }}/>
+                        <Route component={NotFound} />
+                    </Switch>
+                </Router>
+            </div>
+        );
+    }
 }
 
 export default App;
+
+
---
+++ src/components/Resources/ResourceForm.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+
+export default function ResourceForm() {
+    return (
+        <div className="container">
+            <form style={{ color: "black", textAlign: "center" }}>
+                <h2>Add Resource Form</h2>
+            </form>
+        </div>
+    );
+}
+
+
