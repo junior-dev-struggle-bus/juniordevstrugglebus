1cde9368195ea7b727e0568238ea53f64eba9040
Xoadra <xijp77@gmail.com>
2019-11-20 23:59:51 -0800

    Input and select form fields inserted into ResourceForm component page

--- src/components/Resources/ResourceForm.js
+++ src/components/Resources/ResourceForm.js
@@ -7,7 +7,14 @@ import "./ResourceForm.css";
 
 
 export default function ResourceForm() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [subject, setSubject] = useState("");
+    const [category, setCategory] = useState("");
+    const [title, setTitle] = useState("");
+    const [link, setLink] = useState("");
     const [text, setText] = useState("Browser!");
+    const url = "https://forms.gle/CVMbXaZjVk1tnwjDA";
     const netlifyFunction = async event => {
         event.preventDefault();
         if (text === "Browser!") {
@@ -17,19 +24,51 @@ export default function ResourceForm() {
     };
     const googleForm = event => {
         event.preventDefault();
-        window.open("https://forms.gle/CVMbXaZjVk1tnwjDA", "_blank");
+        window.open(url, "_blank");
     };
     return (
         <div className="container">
             <form style={{ color: "black", textAlign: "center" }}>
                 <h2>Add Resource Form</h2>
                 {text}
+                <label>
+                    Name
+                    <input/>
+                </label>
+                <label>
+                    Email
+                    <input/>
+                </label>
+                <label>
+                    Title
+                    <input/>
+                </label>
+                <label>
+                    Subject
+                    <input/>
+                </label>
+                <label>
+                    Category
+                    <select>
+                        <option>Course</option>
+                        <option>Video</option>
+                        <option>Blog Post</option>
+                        <option>Free Course</option>
+                        <option>Inexpensive Course</option>
+                        <option>Bootcamp</option>
+                        <option>Other</option>
+                    </select>
+                </label>
+                <label>
+                    Link
+                    <input/>
+                </label>
                 <div className="resource-submit">
                     <Link to="/resources">
                         <button>Back</button>
                     </Link>
                     <button onClick={netlifyFunction}>Add</button>
-                    <a href="https://forms.gle/CVMbXaZjVk1tnwjDA" target="_blank" rel="noopener noreferrer">
+                    <a href={url} target="_blank" rel="noopener noreferrer">
                         <button onClick={googleForm}>Google</button>
                     </a>
                 </div>
@@ -37,5 +76,6 @@ export default function ResourceForm() {
         </div>
     );
 }
+
 
 
