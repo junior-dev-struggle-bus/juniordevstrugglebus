2400974960dc58f2a98f3f913c416fa277521873
Nate Spilman <nate.spilman@gmail.com>
2020-10-11 14:52:59 -0700

    Update index.js

--- src/components/Resources/index.js
+++ src/components/Resources/index.js
@@ -39,7 +39,7 @@ class Resources extends Component {
   };
   componentDidMount() {
     var resourceData = async () => {
-      const resp = await axios.get("https://natespilman.tech/jdsb/resources/");
+      const resp = await axios.get("https://pioneer-django.herokuapp.com/jdsb/resources/");
       const data = await resp.data;
       const headers = await data[0].map(json =>
         json
